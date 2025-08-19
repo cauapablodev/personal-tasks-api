2c92eab612657a5e8378e4c28a208ad4bbdfb425
Cauã Pablo Santiago Lima <cauapablodev@gmail.com>
2025-08-19 15:32:15 -0300

    chore: create project

---
+++ src/main/java/com/cauapablodev/personaltasksapi/PersonalTasksApiApplication.java
@@ -0,0 +1,13 @@
+package com.cauapablodev.personaltasksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTasksApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalTasksApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cauapablodev/personaltasksapi/PersonalTasksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cauapablodev.personaltasksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTasksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
